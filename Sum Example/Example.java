a3a4299f6844f33524e25128e0a1595ae1206540
dsuthar24 <119012169+dsuthar24@users.noreply.github.com>
2022-12-12 13:41:49 -0500

    Sum Example

---
+++ Sum Example/Example.java
@@ -0,0 +1,19 @@
+
+public class Example {
+	
+    int a;
+    int b;
+	void m1()
+	{
+		int sum=a+b;
+		System.out.println(sum);
+	}
+	public static void main(String args[]) {
+    Example ex=new Example();
+    ex.a=10;
+    ex.b=35;
+    ex.m1();
+	}
+	
+
+}
